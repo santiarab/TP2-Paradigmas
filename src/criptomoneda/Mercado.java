876ced8285e5ed30bd441f4e329dd662598ca37b
santiarab <santiarab7@gmail.com>
2024-06-06 14:48:13 -0300

    mejoras

--- src/criptomoneda/Mercado.java
+++ src/criptomoneda/Mercado.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mercado {
-	private String simbolo, capacidad ,volumen24Hs, var7dias;
-	public Mercado(String simbolo, String capacidad, String volumen24Hs, String var7dias) {
+	private String simbolo, volumen24Hs, var7dias;
+	private float capacidad;
+	public Mercado(String simbolo, float capacidad, String volumen24Hs, String var7dias) {
 		this.simbolo = simbolo;
 		this.capacidad = capacidad;
 		this.var7dias = var7dias;
@@ -13,13 +14,13 @@ public class Mercado {
 	}
 	public static Mercado trozearString(String str) {
 		String[] partes = str.split(",");
-		return new Mercado(partes[0], partes[1], partes[2], partes[3]);
+		return new Mercado(partes[0], Float.parseFloat(partes[1]), partes[2], partes[3]);
 	}
 	public static List<Mercado> trozearString(String[] lista) {
 		List<Mercado> listaCripto = new ArrayList<Mercado>();
 		for (String str : lista) {
 			String[] partes = str.split(",");
-			listaCripto.add(new Mercado(partes[0], partes[1], partes[2], partes[3]));
+			listaCripto.add(new Mercado(partes[0], Float.parseFloat(partes[1]), partes[2], partes[3]));
 		}
 		return listaCripto;
 	}
